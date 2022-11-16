c1489a81712ccd00ddc0017f8f5b2a2f5acc81ee
Johnny <johnny_s.o@hotmail.com>
2022-11-16 06:43:04 -0300

    Ajustando ordem do prevent default para atualizar os dados do cartão de crédito com o inserido no formulário

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const inputNumberCard = document.getElementById('inputNumberCard');
 const inputMM = document.getElementById('inputMM');
 const inputAA = document.getElementById('inputAA');
 const inputCVC = document.getElementById('inputCVC');
-//const button = document.getElementById('btnSubmite');
 
 const cardName = document.getElementById('cardName');
 const cardNumber = document.getElementById('cardNumber');
@@ -13,14 +12,13 @@ const mmCard = document.getElementById('mmCard');
 const aaCard = document.getElementById('aaCard');
 
 
-
 form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  
   cardName.innerText = inputName.value;
   cardNumber.innerText = inputNumberCard.value;
   cvcNumber.innerText = inputCVC.value;
   mmCard.innerText = inputMM.value;
   aaCard.innerText = inputAA.value;
-
-  e.preventDefault();
 });
 
